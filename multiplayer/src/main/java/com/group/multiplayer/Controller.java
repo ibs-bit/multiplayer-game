8b6829892a3a3acfc31aa0e61ccb26a69b3108b6
Ibs <io0om@hotmail.com>
2022-03-10 18:15:41 +0000

    updated controller class

--- multiplayer/src/main/java/com/group/multiplayer/Controller.java
+++ multiplayer/src/main/java/com/group/multiplayer/Controller.java
@@ -1,14 +1,27 @@
 package com.group.multiplayer;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
 
-public class Controller {
-    @FXML
-    private Label welcomeText;
+
+public class Controller extends AnchorPane {
 
     @FXML
-    protected void onButtonClick() {
-        welcomeText.setText("<Dice Roll Argument here>");
+    private TextArea text;
+    @FXML
+    private Button button;
+
+
+    public void initialize() {
+        button.setOnAction(event -> this.onButtonClick());
     }
+
+    @FXML
+    private void onButtonClick() {
+        final int v = (int) (Math.random() * 6 + 1);
+        text.setText(String.valueOf(v));
+    }
+
 }
